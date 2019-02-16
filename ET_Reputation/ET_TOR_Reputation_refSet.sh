624710702212a1e71214647693110b2b4535fa80
Garrett Beasley <garrettbeasley@icloud.com>
2019-02-16 14:51:38 -0800

    Modified ET_TOR_Reputation script for parsing from Firehole instead of directly from the ET rules file.

--- ET_Reputation/ET_TOR_Reputation_refSet.sh
+++ ET_Reputation/ET_TOR_Reputation_refSet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Garrett Beasley - QRadar Emering Threats Intel Importer (RefSet)
+# Garrett Beasley - QRadar ET Firehole Intel Importer (RefSet)
 # Place script in /opt/scripts/ET_Reputation/.
 
 # Download Threat Feeds - Emering Threats Intel
-echo "Downloading Emering Threats Intel"
-wget -nv -O emerging-tor.rules http://rules.emergingthreats.net/blockrules/emerging-tor.rules
+echo "Downloading Emering Threats Intel from Firehole"
+wget -nv -O et_tor.ipset https://iplists.firehol.org/files/et_tor.ipset
 
 # Get script location
 scriptDir=$(pwd)
@@ -15,23 +15,23 @@ fileDateName=$(date +"%Y%m%d_%H%M%S")
 # Reference Set Name
 refSetName=ET_Tor
 
-# Check to see if emerging-tor.rules.old exists
-if [ ! -f emerging-tor.rules.old ]; then
+# Check to see if et_tor.ipset.old exists
+if [ ! -f et_tor.ipset.old ]; then
 {
 	# Notify the user that a old file is not found
 	# Note: This only happens on the first run
 	echo "Old file not found, Creating a placeholder file"
 
 	# Create placeholder file
-	touch emerging-tor.rules.old
+	touch et_tor.ipset.old
 }
 fi
 
-# Check to see if emerging-tor.rules exists (Download Successful?)
-if [ ! -f emerging-tor.rules ]; then
+# Check to see if et_tor.ipset exists (Download Successful?)
+if [ ! -f et_tor.ipset ]; then
 {
 	# Notify the user that a downloaded file was not found
-	echo "The downloaded file (emerging-tor.rules) was not found. Please check the wget command/internet connection."
+	echo "The downloaded file (et_tor.ipset) was not found. Please check the wget command/internet connection."
 
 	# Pause the session for 5 seconds for the user to see the output
 	sleep 5
@@ -41,21 +41,21 @@ if [ ! -f emerging-tor.rules ]; then
 }
 fi
 
-# Check to see if .emerging-tor.refSet exists
+# Check to see if .et_tor.refSet exists
 # This file is responsible for creating the reference set for the first time
 # Note: This file should not get removed, when it's not present the reference set is created.
-if [ ! -f .emerging-tor.refSet ]; then
+if [ ! -f .et_tor.refSet ]; then
 {
   # Notify the user that a old file is not found
   # Note: This only happens on the first run
-	echo ".emerging-tor.refSet was not found, this indicates the refernce set was not created"
+	echo ".et_tor.refSet was not found, this indicates the refernce set was not created"
 	echo "Creating reference set $refSetName"
 
 	# Create placeholder file
-  touch .emerging-tor.refSet
-	echo "# emerging-tor.refSet" >> .emerging-tor.refSet
-  echo "# Created - ($fileDateName)" >> .emerging-tor.refSet
-	echo "# This file is used to let the script know that the reference set ($refSetName) is created already. " >> .emerging-tor.refSet
+  touch .et_tor.refSet
+	echo "# et_tor.refSet" >> .et_tor.refSet
+  echo "# Created - ($fileDateName)" >> .et_tor.refSet
+	echo "# This file is used to let the script know that the reference set ($refSetName) is created already. " >> .et_tor.refSet
 
 	# Create the
 	/opt/qradar/bin/ReferenceSetUtil.sh create "$refSetName" IP
@@ -64,8 +64,8 @@ fi
 
 # Compute MD5 of current and old file
 echo "Computing MD5 sums"
-TIoldMD5=$(md5sum emerging-tor.rules.old | awk '{print $1}')
-TInewMD5=$(md5sum emerging-tor.rules | awk '{print $1}')
+TIoldMD5=$(md5sum et_tor.ipset.old | awk '{print $1}')
+TInewMD5=$(md5sum et_tor.ipset | awk '{print $1}')
 
 # Comparison Logic for threat intel
 if [ "$TIoldMD5" != "$TInewMD5" ]; then
@@ -74,36 +74,30 @@ if [ "$TIoldMD5" != "$TInewMD5" ]; then
   echo "Hashes don't match"
   echo "Parsing new threat intel"
 
-  # Emering Threats Reputation Parsing
+  # Firehole IP List Reputation Parsing
   # Formatting Script (File comes in as a snort rule)
-	# You can disable bits of the threat intel by commenting out each line (# sed -n ***)
-  sed -n 's/.*\[\([^ ].*\)\].*/\1/p' emerging-tor.rules | tr , '\n' | sort -nu >> ET_Tor_Parsed.data
+	# You can disable bits of the threat intel by commenting out each line (# grep -v ***)
+  grep -v '#' et_tor.ipset | awk '{print $1}' | sort -nu > et_tor_parsed.ipset
 
 
   # Overwrite the old threat intel list with the currently parsed one
-  mv -f emerging-tor.rules emerging-tor.rules.old
+  mv -f et_tor.ipset et_tor.ipset.old
 
   # Remove old threat intel from reference set (emerging-tor)
 	# Note: This will remove everything from the reference set
 	echo "Clearing the reference set $refSetName"
   /opt/qradar/bin/ReferenceSetUtil.sh purge "$refSetName"
 
-  # Merge new threat intel into the reference set (ET_Tor)
-	for filename in $scriptDir/ET_Tor_Parsed.data; do
-	    [ -e "$filename" ] || continue
+	# Notify the user that the import is starting for the file/refSetName
+	echo ""
+	echo "Importing the file et_tor_parsed.ipset into the reference set" $refSetName
+	echo ""
 
-			# Notify the user that the import is starting for the file/refSetName
-			echo ""
-			echo "Importing the file $filename" into the reference set $refSetName
-			echo ""
-
-			# Imports data for each ET_*_Parsed.data file found (parsed threat intel)
-			/opt/qradar/bin/ReferenceSetUtil.sh load "$refSetName" $filename
-
-	done
+	# Imports data for each et_tor_parsed.ipset file found (parsed threat intel)
+	/opt/qradar/bin/ReferenceSetUtil.sh load "$refSetName" "$scriptDir/et_tor_parsed.ipset"
 
   # Remote parsed .data files
-  rm -f ET_Tor_Parsed.data
+  rm -f et_tor_parsed.ipset
 
   # Pause the session for 5 seconds for the user to see the output
   sleep 5
@@ -119,7 +113,7 @@ else
   echo "Hashes match, You currently have the latest threat intel."
 
   # Remove the currently downloaded file
-  rm -f emerging-tor.rules
+  rm -f et_tor.ipset
 
   # Pause the session for 5 seconds for the user to see the output
   sleep 5

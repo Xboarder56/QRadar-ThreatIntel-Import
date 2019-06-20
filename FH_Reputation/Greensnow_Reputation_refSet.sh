e65b651090fe5d1fa029cffd80a61e3e9f0b5089
Garrett Beasley <garrettbeasley@icloud.com>
2019-06-20 13:44:04 -0700

    Fixed spelling in old scripts. Added threat list for greensnow (http://iplists.firehol.org/?ipset=greensnow)

--- FH_Reputation/Greensnow_Reputation_refSet.sh
+++ FH_Reputation/Greensnow_Reputation_refSet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Garrett Beasley - QRadar ET Firehol Intel Importer (RefSet)
-# Place script in /opt/scripts/ET_Reputation/.
+# Garrett Beasley - QRadar ET Firehol greensnow Intel Importer (RefSet)
+# Place script in /opt/scripts/FH_Reputation/.
 
-# Download Threat Feeds - Emering Threats Intel
-echo "Downloading Emering Threats Intel from Firehol"
-wget -nv -O et_tor.ipset https://iplists.firehol.org/files/et_tor.ipset
+# Download Threat Feeds - Firehol Threats Intel
+echo "Downloading Threats Intel Feed (greensnow) from Firehol"
+wget -nv -O greensnow.ipset http://blocklist.greensnow.co/greensnow.txt
 
 # Get script location
 scriptDir=$(pwd)
@@ -13,25 +13,25 @@ scriptDir=$(pwd)
 fileDateName=$(date +"%Y%m%d_%H%M%S")
 
 # Reference Set Name
-refSetName=ET_Tor
+refSetName=greensnow
 
-# Check to see if et_tor.ipset.old exists
-if [ ! -f et_tor.ipset.old ]; then
+# Check to see if greensnow.ipset.old exists
+if [ ! -f greensnow.ipset.old ]; then
 {
 	# Notify the user that a old file is not found
 	# Note: This only happens on the first run
 	echo "Old file not found, Creating a placeholder file"
 
 	# Create placeholder file
-	touch et_tor.ipset.old
+	touch greensnow.ipset.old
 }
 fi
 
-# Check to see if et_tor.ipset exists (Download Successful?)
-if [ ! -f et_tor.ipset ]; then
+# Check to see if greensnow.ipset exists (Download Successful?)
+if [ ! -f greensnow.ipset ]; then
 {
 	# Notify the user that a downloaded file was not found
-	echo "The downloaded file (et_tor.ipset) was not found. Please check the wget command/internet connection."
+	echo "The downloaded file (greensnow.ipset) was not found. Please check the wget command/internet connection."
 
 	# Pause the session for 5 seconds for the user to see the output
 	sleep 5
@@ -41,21 +41,21 @@ if [ ! -f et_tor.ipset ]; then
 }
 fi
 
-# Check to see if .et_tor.refSet exists
+# Check to see if .greensnow.refSet exists
 # This file is responsible for creating the reference set for the first time
 # Note: This file should not get removed, when it's not present the reference set is created.
-if [ ! -f .et_tor.refSet ]; then
+if [ ! -f .greensnow.refSet ]; then
 {
   # Notify the user that a old file is not found
   # Note: This only happens on the first run
-	echo ".et_tor.refSet was not found, this indicates the reference set was not created"
+	echo ".greensnow.refSet was not found, this indicates the reference set was not created"
 	echo "Creating reference set $refSetName"
 
 	# Create placeholder file
-  touch .et_tor.refSet
-	echo "# et_tor.refSet" >> .et_tor.refSet
-  echo "# Created - ($fileDateName)" >> .et_tor.refSet
-	echo "# This file is used to let the script know that the reference set ($refSetName) is created already. " >> .et_tor.refSet
+  touch .greensnow.refSet
+	echo "# greensnow.refSet" >> .greensnow.refSet
+  echo "# Created - ($fileDateName)" >> .greensnow.refSet
+	echo "# This file is used to let the script know that the reference set ($refSetName) is created already. " >> .greensnow.refSet
 
 	# Create the
 	/opt/qradar/bin/ReferenceSetUtil.sh create "$refSetName" IP
@@ -64,8 +64,8 @@ fi
 
 # Compute MD5 of current and old file
 echo "Computing MD5 sums"
-TIoldMD5=$(md5sum et_tor.ipset.old | awk '{print $1}')
-TInewMD5=$(md5sum et_tor.ipset | awk '{print $1}')
+TIoldMD5=$(md5sum greensnow.ipset.old | awk '{print $1}')
+TInewMD5=$(md5sum greensnow.ipset | awk '{print $1}')
 
 # Comparison Logic for threat intel
 if [ "$TIoldMD5" != "$TInewMD5" ]; then
@@ -77,27 +77,27 @@ if [ "$TIoldMD5" != "$TInewMD5" ]; then
   # Firehole IP List Reputation Parsing
   # Formatting Script (File comes in as a snort rule)
 	# You can disable bits of the threat intel by commenting out each line (# grep -v ***)
-  grep -v '#' et_tor.ipset | awk '{print $1}' | sort -nu > et_tor_parsed.ipset
+  grep -v '#' greensnow.ipset | awk '{print $1}' | sort -nu > greensnow_parsed.ipset
 
 
   # Overwrite the old threat intel list with the currently parsed one
-  mv -f et_tor.ipset et_tor.ipset.old
+  mv -f greensnow.ipset greensnow.ipset.old
 
-  # Remove old threat intel from reference set (emerging-tor)
+  # Remove old threat intel from reference set
 	# Note: This will remove everything from the reference set
 	echo "Clearing the reference set $refSetName"
   /opt/qradar/bin/ReferenceSetUtil.sh purge "$refSetName"
 
 	# Notify the user that the import is starting for the file/refSetName
 	echo ""
-	echo "Importing the file et_tor_parsed.ipset into the reference set" $refSetName
+	echo "Importing the file greensnow_parsed.ipset into the reference set" $refSetName
 	echo ""
 
-	# Imports data for each et_tor_parsed.ipset file found (parsed threat intel)
-	/opt/qradar/bin/ReferenceSetUtil.sh load "$refSetName" "$scriptDir/et_tor_parsed.ipset"
+	# Imports data for each greensnow_parsed.ipset file found (parsed threat intel)
+	/opt/qradar/bin/ReferenceSetUtil.sh load "$refSetName" "$scriptDir/greensnow_parsed.ipset"
 
   # Remote parsed .data files
-  rm -f et_tor_parsed.ipset
+  rm -f greensnow_parsed.ipset
 
   # Pause the session for 5 seconds for the user to see the output
   sleep 5
@@ -113,7 +113,7 @@ else
   echo "Hashes match, You currently have the latest threat intel."
 
   # Remove the currently downloaded file
-  rm -f et_tor.ipset
+  rm -f greensnow.ipset
 
   # Pause the session for 5 seconds for the user to see the output
   sleep 5
--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This repo contains custom QRadar scripts that I utilize in my home lab to automa
     - AV_Reputation_refSet.sh
   - ET_Reputation
     - ET_TOR_Reputation_refSet.sh
+  - FH_Reputation
+    - Greensnow_refSet.sh
 
 # Cron Job Setup (Example: ET_Tor)
 1. Using SSH, log in to the QRadar Console as the root user.
@@ -23,10 +25,9 @@ This repo contains custom QRadar scripts that I utilize in my home lab to automa
   - As I get more time I will develop addition scripts to parse newer threat feeds. If you have a request leave an issue on the repo with a link to the threat feed download and I will see if I can get the time to parse it!
 
 # Change Log
+  - 06-20-2019 - Fixed spelling in old scripts. Added threat list for greensnow (http://iplists.firehol.org/?ipset=greensnow)
   - 04-14-2019 - Fixed Readme for correct Cronjob steps
-  - 02-16-2019 - Modified ET_TOR_Reputation script for parsing from Firehole instead of directly from the ET rules file.
+  - 02-16-2019 - Modified ET_TOR_Reputation script for parsing from Firehol instead of directly from the ET rules file.
   - 02-02-2019 - Created ET_TOR_Reputation script for importing known TOR nodes/relays. This script will parse the snort rule (.rules) file and upload all IPs to a reference set.
   - 01-29-2019 - Created a reference set version of (AV_Reputation) import script. This allows for avoiding the deploy changes and provides better performance.
   - 01-28-2019 - Initial Upload of AV_Reputation (AlienVault) import script.
-
-# Sources/References
